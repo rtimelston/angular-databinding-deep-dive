c0fda14c49e6476cbf650533029a3509b3f1f24d
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-06-16 10:35:09 -0700

    Chapter 5 - More on View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // means css definitions will be propagated beyond scope of this component
+//   encapsulation: ViewEncapsulation.Native // uses shadow dom with same as Emulated effect, but may not be supported by all browsers
+//   encapsulation: ViewEncapsulation.Emulated // this is the default so need not be used
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: { type: string, name: string, content: string };
